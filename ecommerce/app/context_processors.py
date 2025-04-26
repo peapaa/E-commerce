2e3617327acfb50ae6e6e69b37c3c89df69feab5
thiendd <thienqxa99@gmail.com>
2025-04-26 16:08:19 +0700

    update cart

--- ecommerce/app/context_processors.py
+++ ecommerce/app/context_processors.py
@@ -1,17 +1,15 @@
 from .models import Cart
-
+from .views import check_login
 def global_settings(request):
-    
     return {
         'site_name': 'Neel',
         'cart_item_count': get_cart_item_count(request),  
     }
 
 def get_cart_item_count(request):
-    cart_id = request.session.get('cart_id')
-    if cart_id:
+    cart = check_login(request)
+    if cart:
         try:
-            cart = Cart.objects.get(cart_id=cart_id)
             cart_items = cart.items.all()
         except Cart.DoesNotExist:
             cart_items = []
